76da75622eaa691bc6955fb2331de543049122b9
AngelTM <angel1997amg@gmail.com>
2023-06-30 14:50:29 -0700

    first commit

---
+++ realtionsexample/src/main/java/com/jpa/realtionsexample/RealtionsexampleApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.realtionsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtionsexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtionsexampleApplication.class, args);
+	}
+
+}
---
+++ realtionsexample/src/test/java/com/jpa/realtionsexample/RealtionsexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.realtionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtionsexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
